901593c0c7af6be105c7e09259c5b7a7c8d5ed38
Williams Parra Parra <williams.parra.parra@gmail.com>
2021-04-04 16:49:23 -0400

    Conexion con cloud terminada
    Se modifica conexion a jdbc para trabajar con oracle cloud
    Se genera consulta de prueba a tabla AUTH_PERMISSION

--- src/CONEXION_BD/Conexion.java
+++ src/CONEXION_BD/Conexion.java
@@ -3,7 +3,9 @@ package CONEXION_BD;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 
@@ -14,13 +16,18 @@ public class Conexion {
         
         try{
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            con = (Connection) DriverManager.getConnection("jdbc:oracle:thin:@nomticketdb_high?TNS_ADMIN=//Wallet_NomTicketDB");
+            System.out.println("Intentando conexion");
+            con = (Connection) DriverManager.getConnection("jdbc:oracle:thin:@168.138.146.58:1521:orcl","BDNOMTICKET","@Portafolio2021");
             if (con !=null){
                 System.out.println("Conectado con exito");
+                Statement stmt =con.createStatement();
+                ResultSet rs=stmt.executeQuery("select * from AUTH_PERMISSION");
+                while(rs.next())
+                    System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3) +" "+rs.getString(4));
+                con.close();
             }
-            
         }catch (SQLException e){
-            JOptionPane.showMessageDialog(null,"no se pudo conectar a la base de datos","Error", JOptionPane.ERROR_MESSAGE);
+            System.out.println("fallo la conexion error =" + e.getMessage());           
             
         }
         return con;
