b251c884a9faa75759c21dfdc25e3c8ebd0afba6
Williams Parra Parra <williams.parra.parra@gmail.com>
2021-04-05 23:54:59 -0400

    Merge Branch Main
    Se arregla problema de archivos.

--- src/CONEXION_BD/Conexion.java
+++ src/CONEXION_BD/Conexion.java
@@ -32,5 +32,9 @@ public class Conexion {
         }
         return con;
     }
+
+    public Connection Conectar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
